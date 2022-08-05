2c9ba39a0152a0026c0fc3dc2ea3463c1a854b53
thecicdguy <shyamchitgopkar@gmail.com>
2022-08-05 13:28:03 -0700

    initial version

---
+++ hello.py
@@ -0,0 +1,23 @@
+print('Hello World')
+
+
+# Initialize list
+List = [-999, 'G4G', 1706256, 3.1496, '^_^']
+
+# Show original list
+print("\nOriginal List:\n", List)
+
+
+print("\nSliced Lists: ")
+
+# Modified List
+List[1:1] = ['Geeks', 'for', 'Geeks', '!']
+
+# Display sliced list
+print(List)
+
+# Modified List
+List[:6] = []
+
+# Display sliced list
+print(List)
